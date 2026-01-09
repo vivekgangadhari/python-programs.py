e58505a39e92b60c352e51d100c2c51461ce0f39
GANGADHARI VIVEK <gangadharivivek6@gmail.com>
2026-01-09 16:41:18 +0530

    Add files via upload
    day 2 of learning and completed tasks

---
+++ day2.py
@@ -0,0 +1,102 @@
+#print function usuage
+print("print is used to display output on the screen")
+#it is used incalculating  like:10+10
+print(10+10)
+#hi my name is vivek
+#printing name
+print("vivek")
+#single line comment-->#assigning values to the variable
+student_name="vivek"
+'''this is multi line comment,multi line comment>'''
+#we can also use multi line comment 
+"""for large explanation  purpose we use multi line comments
+assigning value 10 to num_1
+assigning value 10 to num_2
+calculating result
+printing result
+"""
+num_1=10
+num_2=20
+result=num_1+num_2
+print(result)
+#syntax for variable
+number_1=10
+print(number_1)
+number_2=20
+print(number_1+number_2)
+#variables can follow some conditions are
+'''starts with A-Z,a,z,0-9,_'''
+#types of variables
+'''camelCase-->studentName
+snake_case-->student_age
+PaskelCase-->StudentName'''
+_ani=20
+print(_ani)
+#to check the address of variables
+rahul=50
+print(id(rahul))
+#data types in python
+#integer
+a=100
+print(a)
+int_1=200000
+print(int_1)
+print(type(int_1))
+#float
+float=0.67
+print(type(float))
+print(float)
+#string
+str_1="hello world"
+print(type(str_1))
+print(str_1)
+#type conversion
+#int to float
+age=70
+my_conversion=float(age)
+print(my_conversion)
+print(type(my_conversion))
+#string to float
+roll_number="12334"
+int_conversion=float(roll_number)
+print(int_conversion)
+#input function usage
+int_1=int(input("enter the number1:"))
+int_2=int(input("enter the number2:"))
+result=int_1+int_2
+print(result)
+#printing values of variables
+student_age=10
+student_name="rahul"
+print(student_age,student_name)
+#printing pattern using print
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+#different types of data types and printing(int,int,float,string)
+student_age=20
+student_name="vivek"
+student_village="hyderabad"
+student_percentage=79.4
+print(student_age,student_name,student_village,student_percentage)
+#checking the data type of a variable
+person_age=67
+print(type(person_age))
+#displaying the memory address 
+student_name="rahul"
+print(id(student_name))
+#create a prg that takes users age="35",converts it to an integer and prints the result type
+user_age="35"
+integer_conversion=int(user_age)
+print(integer_conversion)
+#string concatination
+first_name="Vivek"
+last_name="Gangadhari"
+result=first_name+" "+last_name
+print(result)
+print(id(first_name))
+print(id(last_name))
+print(type(first_name))
+print(type(last_name))
